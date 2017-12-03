632c85a25b955ee58e4c1edc0a3478d628a1993c
Robert Bravery <robert.bravery@standardbank.co.za>
2017-12-03 21:30:38 +0200

    update react version update redux version completed lecture 41 created books reducer, books container wired up react-redux with connect

---
+++ src/containers/books-list.tsx
@@ -0,0 +1,37 @@
+import * as React  from 'react';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component<{books: any}, {}> {
+  constructor(props) {
+    super(props);
+    
+  }
+  
+renderList() {
+  return this.props.books.map((book) => {
+    return (
+      <li key={book.title} className="list-group-item">
+        {book.title}
+      </li>
+    );
+  });
+}
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  //Whaterver gets return will show up as props inside BookList
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList) ;
---
+++ src/reducers/reducer_books.tsx
@@ -0,0 +1,11 @@
+export default function () {
+  return (
+    [
+      {title: "JavaScript: The good parts"},
+      {title: "C# for beginners"},
+      {title: "Programmers guid"},
+      {title: "MS Best practices"}
+    ]
+  );
+  
+}
